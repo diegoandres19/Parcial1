708f73b1daaa75460b12c749bd493bee6444b33c
Diego Andres <diegobarragan193@gmail.com>
2023-09-21 21:30:03 -0500

    Update EJERCICIO PARCIAL 1.py
    CODIGO MEJOR OPTIMIZADO V2

--- EJERCICIO PARCIAL 1.py
+++ EJERCICIO PARCIAL 1.py
@@ -1,19 +1,17 @@
 numeros = []
-#n = 7+ 3
-n= 10
+n = 10
+
 for i in range(n):
     valor = int(input("Ingrese el valor: "))
     numeros.append(valor)
-for i in range (n):
-    suma = sum(numeros)
-    promedio =int( suma/n)
-print("promedio de la suma es : ", promedio)
+
+suma = sum(numeros)
+promedio = suma / n
+print("Promedio de la suma es:", promedio)
 
 while promedio > 1:
     if promedio % 2 == 0:
-        promedio= promedio/2
-
+        promedio = promedio / 2
     else:
-        promedio=promedio*3 +1
+        promedio = promedio * 3 + 1
     print(promedio)
-    
